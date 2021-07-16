0a35d2dc13b47fedbd10856e64358a2cbd6a6030
Abhay Raj Singh <singh8v@uwindsor.ca>
2021-07-16 20:04:33 +0530

    Add login feature for both seller and user

---
+++ src/main/java/com/tiffin_umbrella/first_release_1/Presentation_Layer/Role.java
@@ -0,0 +1,7 @@
+package com.tiffin_umbrella.first_release_1.Presentation_Layer;
+
+public enum Role {
+    SELLER,
+    USER,
+    ADMIN
+}
--- src/main/java/com/tiffin_umbrella/first_release_1/repository/SellerRepository.java
+++ src/main/java/com/tiffin_umbrella/first_release_1/repository/SellerRepository.java
@@ -1,8 +1,13 @@
 package com.tiffin_umbrella.first_release_1.repository;
+
 import com.tiffin_umbrella.first_release_1.entity.SellerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-@Repository
-public interface SellerRepository extends MongoRepository<SellerEntity,String>{
 
+import java.util.Optional;
+
+@Repository
+public interface SellerRepository extends MongoRepository<SellerEntity, String> {
+
+    Optional<SellerEntity> findByContact_Email(final String email);
 }
