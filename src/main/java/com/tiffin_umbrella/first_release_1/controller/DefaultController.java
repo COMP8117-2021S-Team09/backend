2c7deeec5cabbc57714746c838e8e75a0b398365
Abhay Raj Singh <singh8v@uwindsor.ca>
2021-07-18 00:17:29 +0530

    Add DefaultService for simplicity

--- src/main/java/com/tiffin_umbrella/first_release_1/controller/DefaultController.java
+++ src/main/java/com/tiffin_umbrella/first_release_1/controller/DefaultController.java
@@ -1,14 +1,12 @@
 package com.tiffin_umbrella.first_release_1.controller;
 
 import com.tiffin_umbrella.first_release_1.dto.LoginDetailsDto;
-import com.tiffin_umbrella.first_release_1.dto.Role;
-import com.tiffin_umbrella.first_release_1.entity.BuyerEntity;
-import com.tiffin_umbrella.first_release_1.entity.SellerEntity;
-import com.tiffin_umbrella.first_release_1.repository.BuyerRepository;
-import com.tiffin_umbrella.first_release_1.repository.SellerRepository;
+import com.tiffin_umbrella.first_release_1.service.DefaultService;
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,47 +14,29 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import java.util.UUID;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @CrossOrigin
 @RestController
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @Lazy}))
 public class DefaultController {
 
-    @Autowired
-    private SellerRepository sellerRepository;
-    @Autowired
-    private BuyerRepository buyerRepository;
+    private final DefaultService defaultService;
 
     @Hidden
     @GetMapping(value = {"/", "/docs", "/api-docs"})
     public void defaultLandingController(final HttpServletResponse response) {
-        response.addHeader(HttpHeaders.LOCATION, "/swagger-ui.html");
-        response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
+        defaultService.redirectToApiDocs(response);
     }
 
     @PostMapping(value = "/login",
             consumes = APPLICATION_JSON_VALUE,
             produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<LoginDetailsDto> login(@RequestBody @Valid final LoginDetailsDto loginDetails) {
-        String password;
-        HttpStatus responseStatus = HttpStatus.OK;
-        if (Role.SELLER.equals(loginDetails.getRole())) {
-            password = sellerRepository.findByContact_Email(loginDetails.getEmail())
-                    .orElse(SellerEntity.builder().build()).getPassword();
-        } else {
-            password = buyerRepository.findByContact_Email(loginDetails.getEmail())
-                    .orElse(BuyerEntity.builder().build()).getFirstName();
-        }
-        if (!loginDetails.getPassword().equals(password)) {
-            loginDetails.setMessage("Wrong credentials (email or password mismatch)");
-            loginDetails.setToken(null);
-            responseStatus = HttpStatus.BAD_REQUEST;
-        } else {
-            loginDetails.setMessage("Login successful (email and password valid)");
-            loginDetails.setToken(UUID.randomUUID().toString());
-        }
+    public ResponseEntity<LoginDetailsDto> login(
+            @RequestBody @Valid final LoginDetailsDto loginDetails) {
+        final HttpStatus responseStatus = defaultService.login(loginDetails);
+        log.info("/login attempt with details: {} status: {}", loginDetails, responseStatus);
         return new ResponseEntity<>(loginDetails, responseStatus);
     }
 }
---
+++ src/main/java/com/tiffin_umbrella/first_release_1/service/DefaultService.java
@@ -0,0 +1,50 @@
+package com.tiffin_umbrella.first_release_1.service;
+
+import com.tiffin_umbrella.first_release_1.dto.LoginDetailsDto;
+import com.tiffin_umbrella.first_release_1.dto.Role;
+import com.tiffin_umbrella.first_release_1.entity.*;
+import com.tiffin_umbrella.first_release_1.repository.BuyerRepository;
+import com.tiffin_umbrella.first_release_1.repository.SellerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @Lazy}))
+public class DefaultService {
+
+    private final SellerRepository sellerRepository;
+    private final BuyerRepository buyerRepository;
+
+    public HttpStatus login(final LoginDetailsDto loginDetails) {
+        String password;
+        HttpStatus responseStatus = HttpStatus.OK;
+        if (Role.SELLER.equals(loginDetails.getRole())) {
+            password = sellerRepository.findByContact_Email(loginDetails.getEmail())
+                    .orElse(SellerEntity.builder().build()).getPassword();
+        } else {
+            password = buyerRepository.findByContact_Email(loginDetails.getEmail())
+                    .orElse(BuyerEntity.builder().build()).getFirstName();
+        }
+        if (!loginDetails.getPassword().equals(password)) {
+            loginDetails.setMessage("Wrong credentials (email or password mismatch)");
+            loginDetails.setToken(null);
+            responseStatus = HttpStatus.BAD_REQUEST;
+        } else {
+            loginDetails.setMessage("Login successful (email and password valid)");
+            loginDetails.setToken(UUID.randomUUID().toString());
+        }
+        return responseStatus;
+    }
+
+    public void redirectToApiDocs(final HttpServletResponse response) {
+        response.addHeader(HttpHeaders.LOCATION, "/swagger-ui.html");
+        response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
+    }
+}
