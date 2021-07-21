d5c80ee5115d11630b3f85f650d6112639e9eabd
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-22 01:43:23 +0530

    Add image upload and retrieval APIs

--- src/main/java/com/tiffin_umbrella/first_release_1/common/ErrorCode.java
+++ src/main/java/com/tiffin_umbrella/first_release_1/common/ErrorCode.java
@@ -9,6 +9,11 @@ public enum ErrorCode {
     SELLER_NOT_FOUND_BY_ID,
     SELLER_ALREADY_EXISTS_BY_EMAIL,
 
+    /* image related */
+    IMAGE_NOT_FOUND_BY_ID,
+    IMAGE_SIZE_EXCEEDS_ALLOWED_LIMIT_IN_MB,
+    IMAGE_SIZE_EXCEEDS_ALLOWED_DIMENSIONS,
+
     /* plan related */
     PLAN_NOT_FOUND_BY_ID
 }
